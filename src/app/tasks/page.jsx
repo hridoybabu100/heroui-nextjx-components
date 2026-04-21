d8d858e4a94414361eaa99ea625c0badd56dbe81
Hridoy Hasan Babu <hridoyakanda100@gmail.com>
2026-04-21 21:43:25 +0600

    modal updated

--- src/app/tasks/page.jsx
+++ src/app/tasks/page.jsx
@@ -1,4 +1,5 @@
 
+import { Addtask } from "@/components/Modal/Addtask";
 import TasksCard from "@/components/tasksCard/TasksCard";
 import { gettasks } from "@/lib/tasks";
 
@@ -11,7 +12,9 @@ const taskspage = async () => {
       <h2 className="text-4xl py-10 font-bold text-white">
         All tasks : {tasks.length}
       </h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+
+      <Addtask></Addtask>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 py-10">
         {
           tasks.map((task) =>  <TasksCard key={task.id} task={task}></TasksCard>)
         }
